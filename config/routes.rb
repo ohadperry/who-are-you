d957a747793e944fa7e068e18fea7e65e2a3158f
ohad partuck <ohadpartuck@gmail.com>
2014-03-15 21:37:17 +0200

    facebook connect

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ WhoAreYou::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'application#index'
+  get '/',                                     to: 'application#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
