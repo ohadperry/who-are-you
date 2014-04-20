0d5d5b58a81d0bb8e3e2a5b318ac0c5bb39c31b1
ohad partuck <ohadpartuck@gmail.com>
2014-04-20 08:34:44 +0300

    adding user id to the payload

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationsController < ApplicationController
 
   def respond
 
-    payload = {auth_result: params[:auth_result]}
+    payload = {auth_result: params[:auth_result], user_id: params[:user_id]}
     res = Authentication.log_in_or_create(params[:provider], payload)
     render json: res, layout: false
   end
